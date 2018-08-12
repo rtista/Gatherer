d37a248e0172b12f656284d6423c1e7f7e1895f2
WorkingTitus <workingtitus@gmail.com>
2018-08-13 00:54:04 +0100

    Listener now allows spawning and stoping process instances.

--- cli/supervisor/listener.py
+++ cli/supervisor/listener.py
@@ -10,21 +10,50 @@ class UnixListener(Thread):
     Args:
         Thread (threading.Thread): Extends the Thread class for asychronous execution.
     """
-    def __init__(self, sock, monitormap):
+    def __init__(self, sock, children):
         """
         Creates the object.
         
         Args:
             sock (multiprocessing.connection.Listener): The Listener object.
-            monitormap (dict): The mapping of consumers to monitored count.
+            children (dict): The mapping of the supervisor assigned processes.
         """
         Thread.__init__(self)
         self.sock = sock
-        self.monitormap = monitormap
+        self.children = children
+
+    def spawnProcess(self, process, dif):
+        """
+        Starts a process if not running.
+        
+        Args:
+            process (str): The name of the process to start.
+            dif (int): Number of instances of the process to spawn.
+        """
+        # Fail if process not in
+        if process not in self.children.keys():
+            return
+        
+        self.children[process]['monitored'] += dif
+
+        if self.children[process]['monitored'] < 0:
+            self.children[process]['monitored'] = 0
+
+    def stopProcess(self, process):
+        """
+        Stops all instances of a process.
+        
+        Args:
+            process (str): The name of the process to start.
+        """
+        if process not in self.children.keys():
+            return
+
+        self.children[process]['monitored'] = 0
 
     def run(self):
         """
-        Listens for commands from a unix_af listener.s
+        Listens for commands from a UNIX_AF listener.
         """
         # Endless Loop
         while True:
@@ -37,8 +66,10 @@ class UnixListener(Thread):
 
                     # Reads content from socket
                     op = connection.recv()
-
+                    
                     # Action
-                    for elem in self.monitormap.keys():
-                        if elem.__name__ == op[1]:
-                            self.monitormap[elem] += int(op[2])
+                    if op[0] == 'spawn':
+                        self.spawnProcess(op[1], int(op[2]))
+                        
+                    elif op[0] == 'stop':
+                        self.stopProcess(op[1])
