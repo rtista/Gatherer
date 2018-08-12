5d22fb1d5a920e51cae6710bd09107b985dea771
WorkingTitus <workingtitus@gmail.com>
2018-08-13 00:54:23 +0100

    gatherer-ctl cli working properly.

--- cli/gatherer-ctl.py
+++ cli/gatherer-ctl.py
@@ -22,25 +22,19 @@ def stop():
     '''
     Stops the gatherer-cli supervisor process.
     '''
-    pid = None
-
+    # Read Supervisor PID from pidfile
     try:
         with open(AppConfig.PID_LOCATION, 'r') as pidfile:
             pid = int(pidfile.readline())
 
+        # Try to kill process
+        kill(pid, SIGTERM)
+
     except FileNotFoundError:
         print('Could not find the {} file.'.format(AppConfig.PID_LOCATION))
-        exit(1)
 
-    if pid is not None:
-
-        # Try to kill process
-        try:
-            kill(pid, SIGTERM)
-
-        except ProcessLookupError:
-            print('Could not find a process with the given PID. Stale PID file?')
-            exit(1)
+    except ProcessLookupError:
+        print('Could not find a process with the given PID. Stale PID file?')
 
 
 def start():
@@ -48,11 +42,7 @@ def start():
     Starts the gatherer-cli supervisor process.
     '''
     # Create Supervisor
-    supervisor = ConsumerSupervisor()
-
-    # Assign Consumers
-    for consumer in ASSIGNED_CONSUMERS.keys():
-        supervisor.assignConsumer(consumer, ASSIGNED_CONSUMERS[consumer])
+    supervisor = ConsumerSupervisor(ASSIGNED_CONSUMERS)
 
     # Start the supervisor process
     supervisor.start()
@@ -66,16 +56,6 @@ def restart():
     start()
 
 
-def startConsumer(consumer):
-    '''
-    Start the consumers of the given class.
-    
-    Args:
-        args (list): Arguments to be passed onto the function.
-    '''
-    raise NotImplementedError('Not yet implemented')
-
-
 def stopConsumer(consumer):
     '''
     Stops all the consumers of the given class.
@@ -83,7 +63,11 @@ def stopConsumer(consumer):
     Args:
         args (list): Arguments to be passed onto the function.
     '''
-    raise NotImplementedError('Not yet implemented')
+    client = Client(AppConfig.UNIX_SOCKET, 'UNIX_AF')
+
+    client.send(['stop', consumer])
+
+    client.close()
 
 
 def spawn(consumer, dif):
@@ -99,7 +83,6 @@ def spawn(consumer, dif):
 
     client.close()
 
-
 # Available operations
 OPERATIONS = {
     'stop': stop,
@@ -107,7 +90,6 @@ OPERATIONS = {
     # 'reload': reload,
     'restart': restart,
     'consumer': {
-        'start': startConsumer,
         'stop': stopConsumer,
         'spawn': spawn,
     }
@@ -121,14 +103,14 @@ if __name__ == '__main__':
 
     arglen = len(argv)
     
-    if arglen == 2: 
+    if arglen == 2:
         OPERATIONS[argv[1]]()
-    
-    elif arglen == 4: 
+        
+    elif arglen == 4:
         OPERATIONS[argv[1]][argv[2]](argv[3])
     
     elif arglen == 5:
         OPERATIONS[argv[1]][argv[2]](argv[3], argv[4])
     
     else:
-        print('Missing')
+        print('Invalid Command')
