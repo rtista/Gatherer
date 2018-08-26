ac7997639e1d7ed5f0cc8edd6bd02009e8b9bfc9
WorkingTitus <workingtitus@gmail.com>
2018-08-26 23:57:27 +0100

    Removed adapter logic from consumer.

--- cli/consumer/consumer.py
+++ cli/consumer/consumer.py
@@ -28,27 +28,6 @@ class QueueConsumer(Process):
         """
         self.sigmap[sig] = sighandler
 
-    def connect(self):
-        """
-        Connects to the MQ system using the conn_conf parameter.
-        
-        Raises:
-            NotImplementedError: This is an abstract function.
-        """
-        raise NotImplementedError('This is an abstract function.')
-
-    def subscribe(self, client):
-        """
-        Subscribes to the designated queue.
-        
-        Args:
-            client (Object): The MQ system connection client.
-        
-        Raises:
-            NotImplementedError: This is an abstract function.
-        """
-        raise NotImplementedError('This is an abstract function.')
-
     def can_consume(self):
         """
         Returns whether the consumer should consume messages or stop execution.
@@ -57,18 +36,6 @@ class QueueConsumer(Process):
             Boolean : Whether the consumer should consume messages or stop (default is True)
         """
         return True
-
-    def receive(self, client):
-        """
-        Gets a message from the message system and returns it.
-        
-        Args:
-            client (Object): The MQ system connection client.
-
-        Raises:
-            NotImplementedError: This is an abstract function.
-        """
-        raise NotImplementedError('This is an abstract function.')
 
     def consume(self, message):
         """
@@ -81,44 +48,6 @@ class QueueConsumer(Process):
         Returns:
             Boolean: Whether the message should or not be acknowledged.
 
-        Raises:
-            NotImplementedError: This is an abstract function.
-        """
-        raise NotImplementedError('This is an abstract function.')
-
-    def ack(self, client, message):
-        """
-        Acknowledges the given message.
-        
-        Args:
-            client (Object): The MQ system connection client.
-            message (Object): A MQ system message.
-
-        Raises:
-            NotImplementedError: This is an abstract function.
-        """
-        raise NotImplementedError('This is an abstract function.')
-
-    def nack(self, client, message):
-        """
-        Not acknowledges the given message.
-        
-        Args:
-            client (Object): The MQ system connection client.
-            message (Object): A MQ system message.
-
-        Raises:
-            NotImplementedError: This is an abstract function.
-        """
-        raise NotImplementedError('This is an abstract function.')
-
-    def disconnect(self, client):
-        """
-        Disconnects from the Message Queueing System.
-        
-        Args:
-            client (Object): The MQ system connection client.
-        
         Raises:
             NotImplementedError: This is an abstract function.
         """
